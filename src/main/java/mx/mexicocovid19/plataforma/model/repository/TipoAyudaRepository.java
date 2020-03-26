5a8e2bec76c85079cfa003db71952f69621a8a5a
Roberto Salvador Olguin Lozano <betotsol@gmail.com>
2020-03-26 13:55:23 -0600

    add new service to tipoAyuda catalog

---
+++ src/main/java/mx/mexicocovid19/plataforma/model/repository/TipoAyudaRepository.java
@@ -0,0 +1,7 @@
+package mx.mexicocovid19.plataforma.model.repository;
+
+import mx.mexicocovid19.plataforma.model.entity.TipoAyuda;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TipoAyudaRepository extends JpaRepository<TipoAyuda, Integer> {
+}
