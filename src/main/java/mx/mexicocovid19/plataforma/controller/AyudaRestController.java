d8ef5fefd94bb30f70c9908dd2a25fd7d07b1df5
Roberto Salvador Olguin Lozano <betotsol@gmail.com>
2020-03-25 19:45:41 -0600

    delete

--- src/main/java/mx/mexicocovid19/plataforma/controller/AyudaRestController.java
+++ src/main/java/mx/mexicocovid19/plataforma/controller/AyudaRestController.java
@@ -2,11 +2,8 @@ package mx.mexicocovid19.plataforma.controller;
 
 import mx.mexicocovid19.plataforma.ApiController;
 import mx.mexicocovid19.plataforma.controller.dto.AyudaDTO;
-import mx.mexicocovid19.plataforma.controller.dto.MunicipalityDTO;
 import mx.mexicocovid19.plataforma.controller.mapper.AyudaMapper;
-import mx.mexicocovid19.plataforma.controller.mapper.MunicipalityMapper;
 import mx.mexicocovid19.plataforma.service.AyudaService;
-import mx.mexicocovid19.plataforma.service.MunicipalityService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -24,8 +21,6 @@ public class AyudaRestController {
     @Autowired
     private AyudaService ayudaService;
 
-    @Autowired
-    private MunicipalityService municipalityService;
 
     @ResponseBody
     @GetMapping(
@@ -36,13 +31,5 @@ public class AyudaRestController {
                                      @RequestParam(value = "latitude") final BigDecimal latitude,
                                      @RequestParam(value = "kilometers") final Integer kilometers) {
         return AyudaMapper.from(ayudaService.readAyudas(origenAyuda, longitude, latitude, kilometers));
-    }
-
-    @ResponseBody
-    @GetMapping(
-            value = { "/province/{province}/municipality" },
-            produces = {"application/json;charset=UTF-8"})
-    public List<MunicipalityDTO> readMunicipalitiesByProvince(@PathVariable(value = "province") Integer idProvince) {
-        return MunicipalityMapper.from(municipalityService.readMunicipalitiesByProvince(idProvince));
     }
 }
