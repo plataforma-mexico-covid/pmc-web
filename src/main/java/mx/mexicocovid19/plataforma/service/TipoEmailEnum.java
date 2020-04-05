eec9f1fb1203ca2c2c5c6cfcb7ae4b19fbedf26d
Karen Kim Vazquez Angeles <kkimvazquezangeles@Karens-MacBook-Air.local>
2020-04-05 14:21:06 -0500

    email-asunto

--- src/main/java/mx/mexicocovid19/plataforma/service/TipoEmailEnum.java
+++ src/main/java/mx/mexicocovid19/plataforma/service/TipoEmailEnum.java
@@ -1,11 +1,11 @@
 package mx.mexicocovid19.plataforma.service;
 
 public enum TipoEmailEnum {
-    REGISTRO_USUARIO("email/registroUsuario.vm", "Bienvenido a bordo!"),
-    OFRECE_AYUDA("email/ofreceAyuda.vm", "Gracias por sumar!"),
-    SOLICITA_AYUDA("email/solicitaAyuda.vm", "Gracias tu solicitud fue registrada!"),
-    MATCH_AYUDA("email/matchAyuda.vm", "Es momento de hacer equipo!"),
-    RECUPERACION_PASSWORD("email/recoveryPassword.vm", "Recuperacion de password!"),;
+    REGISTRO_USUARIO("email/registroUsuario.vm", "¡Bienvenido a bordo!"),
+    OFRECE_AYUDA("email/ofreceAyuda.vm", "¡Gracias por sumar!"),
+    SOLICITA_AYUDA("email/solicitaAyuda.vm", "¡Gracias tu solicitud fue registrada!"),
+    MATCH_AYUDA("email/matchAyuda.vm", "¡Es momento de hacer equipo!"),
+    RECUPERACION_PASSWORD("email/recoveryPassword.vm", "Recuperar password"),;
 
     private String template;
     private String subject;
